178d3170341f1d52862296a0aeb27368a891672a
Rengkat <successinmotion11@gmail.com>
2024-07-06 05:55:16 +0100

    add logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,23 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
 import { IoMenu } from "react-icons/io5";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
+import { removeUserFromLocalStorage } from "../contexts/localStorage";
 interface Props {
   search: string;
   setSearch: (value: string) => void;
 }
 const NavBar = ({ search, setSearch }: Props) => {
+  const navigate = useNavigate();
+  const [logoutBtn, setLogoutBtn] = useState(false);
   const { closeSideBar, user } = useContext(AppContext);
+  const handleLogout = () => {
+    removeUserFromLocalStorage();
+    navigate("/login");
+    window.location.reload();
+  };
   return (
     <div className="nav-container">
       <nav>
@@ -38,11 +47,17 @@ const NavBar = ({ search, setSearch }: Props) => {
             <IoIosNotifications className="notification-icon" />
             <div className="notice"></div>
           </div>
-          <div className="profile-nav">
+          <div onClick={() => setLogoutBtn((prev) => !prev)} className="profile-nav">
             <FaRegUserCircle className="profile-icon" />
 
             <h5 className="name">{user?.username}</h5>
             <MdKeyboardArrowDown className="profile-icon" />
+            {logoutBtn && (
+              <div onClick={handleLogout} className="logout-btn">
+                {" "}
+                Logout
+              </div>
+            )}
           </div>
         </div>
       </nav>
